62516f739981cc3b68800538be14a4d5a5986a60
Alex Stanciu <alex@auth0.com>
2021-04-13 22:45:28 -0400

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: 'Digital Couture'
+    title: "Digital Couture",
   },
   proxy: {
     prefix: "/api",
@@ -12,29 +12,29 @@ module.exports = {
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
+        pathToConfigModule: `src/utils/typography.js`,
+      },
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.SPACE_ID,
-        accessToken: process.env.ACCESS_TOKEN
-      }
+        accessToken: process.env.ACCESS_TOKEN,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images/`
-      }
+        path: `${__dirname}/src/images/`,
+      },
     },
     {
       resolve: `gatsby-plugin-sharp`,
       options: {
         useMozJpeg: false,
-        stripMetadata: true
-      }
+        stripMetadata: true,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
@@ -42,12 +42,8 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-69050-11",
-        head: false,
-        respectDNT: true,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        cookieDomain: "digitalcouture.com",
+        head: true,
       },
     },
-  ]
+  ],
 };
